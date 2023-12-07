782535ceabc7a479ccf5653b68072a8b214b39ae
Debayan Kar <kardebayan3@gmail.com>
2023-12-07 12:31:56 +0530

    RMX2001L1: AOSP-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The PixelOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2001L1.mk
+    $(LOCAL_DIR)/aosp_RMX2001L1.mk
---
+++ aosp_RMX2001L1.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The PixelOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+$(call inherit-product, device/realme/RMX2001L1/full_RMX2001L1.mk)
+
+# Inherit common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_RMX2001L1
